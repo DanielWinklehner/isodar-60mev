58bebc5a5bb59118bf5053d15dacfc9578293592
Daniel Winklehner <winklehn@mit.edu>
2020-09-21 19:16:11 -0400

    Upload of Devin's latest optimization -DW

---
+++ 2020-09-21 (optimized by Devin)/setenv.sh
@@ -0,0 +1,6 @@
+export TEMPLATES=/nobackup1/winklehn/isodar/harm4/tmpl/
+export FIELDMAPS=/nobackup1/winklehn/isodar/fieldmaps
+export DISTRIBUTIONS=/nobackup1/winklehn/isodar/distributions
+export SLURM_PARTITION=sched_mit_psfc
+export SLURM_RAM=64
+export SLURM_TIME=12:00:00
